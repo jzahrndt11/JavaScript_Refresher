9cef40fd6946dbc6e895e5827e3cace0e58a45e8
jez87y <jez87y@umsystem.edu>
2024-01-23 23:31:12 -0600

    Initial commit 	new file:   index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log('Hello, world')
+
+// Declaring Variables
+const SIZE = 1000 // stands for constant makes the variable immutable
+let myVar = 10 // is a mutable object and are allowed to change it
+// The difference between const and let is mutability 
+myVar = 11
+var c; // DONT USE: can lead to some weird issues when writing modern javaScript
+console.log(myVar)
+
+// Primitive Types
+//Number // number includes integer, floats
+//BigInt // used for scientific programs 
+//String
+//undefined // no value whatsoever, it hasn't been allocated (Try to avoid)
+//null // has been allocated but simply means there is no value
+
+// Reference Types
+
+
+
+// Truthy and falsy values (and a few javascript quirks)
+
+
+
+// Function declarations
